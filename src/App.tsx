a050acf64cd3832129d00c33b172bb696efa7e46
zeenyyy <peterkc50@hotmail.com>
2026-03-27 15:07:22 +0700

    4.0.2 username

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </div>
         <div>
           <h1>I JET K</h1>
-          <h1>version 4.0.1</h1>
+          <h1>version 4.0.2</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
